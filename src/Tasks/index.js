43ccbfd69bdaaee9430b7c490f866193dbaa3255
Aleksandra Jablonska <jablonska.aleksandra96@gmail.com>
2023-05-19 14:10:15 +0200

    Key to list item added

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./style.css";
 
-const Tasks = ({tasks, hideDone }) => (
-<ul className="tasks">
-    {tasks.map(task => (
-        <li
-        className={`tasks__item ${task.done && hideDone ? "tasks__item--hidden" : ""}`}>
-            
-            <button className="tasks__button tasks__button--done">{task.done ? "✔" : ""}</button>
-                <span className={`${task.done ? "tasks__content--done" : ""}`}>{task.content}</span>
-                <button className="tasks__button tasks__button--remove">🗑</button>
-        </li>
+const Tasks = ({ tasks, hideDone }) => (
+  <ul className="tasks">
+    {tasks.map((task) => (
+      <li key={task.id}
+        className={`tasks__item ${
+          task.done && hideDone ? "tasks__item--hidden" : ""
+        }`}
+      >
+        <button className="tasks__button tasks__button--done">
+          {task.done ? "✔" : ""}
+        </button>
+        <span className={`${task.done ? "tasks__content--done" : ""}`}>
+          {task.content}
+        </span>
+        <button className="tasks__button tasks__button--remove">🗑</button>
+      </li>
     ))}
-</ul>
+  </ul>
 );
 
 export default Tasks;
